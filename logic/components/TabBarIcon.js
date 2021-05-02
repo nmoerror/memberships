a1adbf4575ef037809e8cfc2555df0ec89ba3163
Ale <aleeeeeehs@icloud.com>
2021-05-02 12:22:40 +1000

    [master] fixed button size props for consistency

--- logic/components/TabBarIcon.js
+++ logic/components/TabBarIcon.js
@@ -2,8 +2,15 @@ import { Ionicons } from '@expo/vector-icons';
 import * as React from 'react';
 import Colors from '../constants/Colors';
 
-export default function TabBarIcon({ name, focused }) {
+export default function TabBarIcon({ name, focused, size }) {
   let color = focused ? Colors.tabIconSelected : Colors.tabIconDefault;
 
-  return <Ionicons name={name} size={35} color={color} />;
+  return <Ionicons name={name} size={size} color={color} style={{
+    display: 'block',
+  }}
+  />;
+}
+
+TabBarIcon.defaultProps = {
+  size: 10
 }
--- navigation/BottomTabNavigation.js
+++ navigation/BottomTabNavigation.js
@@ -4,42 +4,43 @@ import * as React from 'react';
 import HomeScreen from '../logic/screens/HomeScreen';
 import StatisticsScreen from '../logic/screens/StatisticsScreen';
 import MenuScreen from '../logic/screens/MenuScreen';
+
 const BottomTab = createBottomTabNavigator();
 const INITIAL_ROUTE_NAME = 'Statistics';
 
 export default function BottomTabNavigator() {
-	return (
-		<BottomTab.Navigator initialRouteName={INITIAL_ROUTE_NAME}>
-			<BottomTab.Screen
-				name="Home"
-				component={HomeScreen}
-				options={{
-					title: "Expenses",
-					tabBarIcon: ({ focused }) => (
-						<TabBarIcon focused={focused} name="md-list" />
-					),
-				}}
-			/>
-			<BottomTab.Screen
-				name="Statistics"
-				component={StatisticsScreen}
-				options={{
-					title: "Totals",
-					tabBarIcon: ({ focused }) => (
-						<TabBarIcon focused={focused} name="receipt" />
-					),
-				}}
-			/>
-			<BottomTab.Screen
-				name="Menu"
-				component={MenuScreen}
-				options={{
-					title: "Menu",
-					tabBarIcon: ({ focused }) => (
-						<TabBarIcon focused={focused} name="md-menu" />
-					),
-				}}
-			/>
-		</BottomTab.Navigator>
-	);
+  return (
+    <BottomTab.Navigator initialRouteName={INITIAL_ROUTE_NAME}>
+      <BottomTab.Screen
+        name='Home'
+        component={HomeScreen}
+        options={{
+          title: 'Expenses',
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon focused={focused} name='md-list' size={35}/>
+          ),
+        }}
+      />
+      <BottomTab.Screen
+        name='Statistics'
+        component={StatisticsScreen}
+        options={{
+          title: 'Totals',
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon focused={focused} name='receipt' size={30}/>
+          ),
+        }}
+      />
+      <BottomTab.Screen
+        name='Menu'
+        component={MenuScreen}
+        options={{
+          title: 'Menu',
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon focused={focused} name='ios-menu' size={40}/>
+          ),
+        }}
+      />
+    </BottomTab.Navigator>
+  );
 }
